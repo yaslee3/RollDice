ffc16ae7d35d4073fcf40024c3b68c8ccf391461
Yasmine Lee <yaslee3@yahoo.com>
2016-09-10 23:39:09 -0400

    inital comment commit

---
+++ src/Labfive/MagicDiceRollApp.java
@@ -0,0 +1,53 @@
+package Labfive;
+
+public class MagicDiceRollApp {
+
+	// write the main() method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// declare and initialize instance variables.
+
+		// create a Scanner object called 'scan' for gathering user input.
+		
+		
+
+		
+		//prompt the user to make a choice whether to roll the dice or not. 
+		
+		
+		
+		
+		// store the user input in the String variable 'choice'
+		
+		
+		
+
+		//create a 'while-loop' when the user selects 'y' when prompted to roll the dice. 
+		
+		
+
+		// prompt the user to select how many sides the die will have.
+
+		
+		// store the user input for the number of sides into the variable 'sides'
+		
+		
+		
+		// create new instances of the Magic Dice 
+		
+		
+		// create local int variables 'result1' and 'result2' 
+		 
+		
+		// create a  'for-loop', set the int variable 'i' to 1, 
+
+
+		// create 'if/else-statement'
+
+		
+
+	}
+
+	
+}
---
+++ src/Labfive/MagicRollDice.java
@@ -0,0 +1,36 @@
+package Labfive;
+
+public class MagicRollDice {
+
+	
+	// declare the instance varaiables and make private
+	
+	
+	
+	
+	//initialize your variables inside of the constructor 
+	
+	
+	
+	
+	//create the setter method with keyword void This method doesn't return data
+
+
+	
+	
+	//create getter method this method returns data 
+	
+	
+	
+	
+	
+	
+	
+	//create the 'rollDice' method.
+    
+	//declare, and then create a return statement in the body of the method
+
+	
+	
+}
+
