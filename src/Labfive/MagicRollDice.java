5fe322ba6accb8a3b4995c6b68116550856cec36
Yasmine Lee <yaslee3@yahoo.com>
2016-09-13 09:32:25 -0400

    commit and added challenge part

--- src/Labfive/MagicRollDice.java
+++ src/Labfive/MagicRollDice.java
@@ -5,29 +5,62 @@ public class MagicRollDice {
 	
 	// declare the instance varaiables and make private
 	
-	
+	private int sides =0;
+	private int roll=0;
 	
 	
 	//initialize your variables inside of the constructor 
 	
+	public MagicRollDice() { 
+		 		sides = 0; roll = 0;
+		  	}
+
+
 	
-	
-	
+
 	//create the setter method with keyword void This method doesn't return data
 
+	public void setSides(int sides) { 
+		 		this.sides = sides; 
+		}
+	
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
 
-	
-	
 	//create getter method this method returns data 
 	
-	
-	
-	
-	
-	
+	public int getRoll() {
+		return roll;
+	}
+
+	public int getSide() {
+		return sides;
+	}
+
+
 	
 	//create the 'rollDice' method.
     
+	public int rollDice(int s) { 
+		 		roll = (int) Math.ceil(Math.random() * sides);
+		  		return roll; 
+		 	}
+		 
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//declare, and then create a return statement in the body of the method
 
 	
